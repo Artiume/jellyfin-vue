826ecbb36a20ce65d7ace16a9b8d9f884cf07e09
MrTimscampi <julien.machiels@protonmail.com>
2020-12-25 22:16:22 +0100

    fix(player-manager): fix scroll when a player is open

---
+++ components/PlayerDialog.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Vue from 'vue';
+import { VDialog } from 'vuetify/lib';
+
+export default Vue.extend({
+  extends: VDialog,
+  methods: {
+    // Copy of https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/mixins/overlayable/index.ts#L129
+    // A block preventing events on the overlay has been removed.
+    shouldScroll() {
+      return true;
+    }
+  }
+});
+</script>
